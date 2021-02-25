783f27ff97ff8fc5724e8e4caf36de540650ea73
张浩材 <435504339@qq.com>
2021-02-26 02:21:41 +0800

    修改代码

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -14,22 +14,30 @@ public class Test : MonoBehaviour
         if(Input.GetButton("w"))
         {
             // m_RectTransform.anchoredPosition = m_RectTransform.anchoredPosition + new Vector2(0, 1);
-            m_RectTransform.anchorMax = m_RectTransform.anchorMax + new Vector2(0, 0.01f);
+            // m_RectTransform.anchorMax = m_RectTransform.anchorMax + new Vector2(0, 0.01f);
+            // m_RectTransform.localPosition = new Vector3(0, 100, 0);
+            m_RectTransform.anchoredPosition = new Vector2(0, 100);
         }
         else if(Input.GetButton("s"))
         {
             // m_RectTransform.anchoredPosition = m_RectTransform.anchoredPosition + new Vector2(0, -1);
-            m_RectTransform.anchorMax = m_RectTransform.anchorMax + new Vector2(0, -0.01f);
+            // m_RectTransform.anchorMax = m_RectTransform.anchorMax + new Vector2(0, -0.01f);
+            // m_RectTransform.localPosition = new Vector3(0, -100, 0);
+            m_RectTransform.anchoredPosition = new Vector2(0, -100);
         }
         else if (Input.GetButton("a"))
         {
             // m_RectTransform.anchoredPosition = m_RectTransform.anchoredPosition + new Vector2(-1, 0);
-            m_RectTransform.anchorMax = m_RectTransform.anchorMax + new Vector2(-0.01f, 0);
+            // m_RectTransform.anchorMax = m_RectTransform.anchorMax + new Vector2(-0.01f, 0);
+            // m_RectTransform.localPosition = new Vector3(-100, 0, 0);
+            m_RectTransform.anchoredPosition = new Vector2(-100, 0);
         }
         else if (Input.GetButton("d"))
         {
             // m_RectTransform.anchoredPosition = m_RectTransform.anchoredPosition + new Vector2(1, 0);
-            m_RectTransform.anchorMax = m_RectTransform.anchorMax + new Vector2(0.01f, 0);
+            // m_RectTransform.anchorMax = m_RectTransform.anchorMax + new Vector2(0.01f, 0);
+            // m_RectTransform.localPosition = new Vector3(100, 0, 0);
+            m_RectTransform.anchoredPosition = new Vector2(100, 0);
         }
     }
 
@@ -44,5 +52,7 @@ public class Test : MonoBehaviour
         GUI.Label(new Rect(0, 150, 500, 50), "AnchorMin : " + m_RectTransform.anchorMin.ToString(), style);
         GUI.Label(new Rect(0, 200, 500, 50), "Left : " + m_RectTransform.offsetMin.x.ToString() + " Bottom : " + m_RectTransform.offsetMin.y.ToString(), style);
         GUI.Label(new Rect(0, 250, 500, 50), "Right : " + (-m_RectTransform.offsetMax.x).ToString() + " Top : " + (-m_RectTransform.offsetMax.y).ToString(), style);
+        GUI.Label(new Rect(0, 300, 500, 50), "rect:" + m_RectTransform.rect, style);
+        GUI.Label(new Rect(0, 350, 500, 50), "localPosition:" + m_RectTransform.localPosition.ToString(), style);
     }
 }
